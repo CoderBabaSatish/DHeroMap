2f07800f8f4484758eeea5628fa8c6679b66bd98
Satish <47214559+CoderBabaSatish@users.noreply.github.com>
2019-03-19 15:10:29 +0400

    Add files via upload

---
+++ DHeroMap/DHeroMapTests/DHeroMapTests.swift
@@ -0,0 +1,34 @@
+//
+//  DHeroMapTests.swift
+//  DHeroMapTests
+//
+//  Created by Satish on 3/19/19.
+//  Copyright © 2019 Satish. All rights reserved.
+//
+
+import XCTest
+@testable import DHeroMap
+
+class DHeroMapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
